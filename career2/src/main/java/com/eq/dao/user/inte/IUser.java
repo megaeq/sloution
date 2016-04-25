36bc14e168a902346031bf417b96ba10eb1e1b65
wisdom <wisdom@172.16.1.109>
2016-04-25 16:11:45 +0800

    201604251611

--- career2/src/main/java/com/eq/dao/user/inte/IUser.java
+++ career2/src/main/java/com/eq/dao/user/inte/IUser.java
@@ -1,9 +1,11 @@
 package com.eq.dao.user.inte;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eq.dao.user.entity.User;
 
 public interface IUser extends JpaRepository<User, Long>  {
-
+	public Page<User> findByUserName(String userName,Pageable pageable);
 }
--- career2/src/test/java/com/eq/user/UserTest.java
+++ career2/src/test/java/com/eq/user/UserTest.java
@@ -1,15 +1,23 @@
 package com.eq.user;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.eq.dao.user.entity.User;
-import com.eq.dao.user.inte.UserImpl2;
+import com.eq.dao.user.inte.IUser;
 import com.eq.service.UserService;
+
+import net.sf.json.JSONObject;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext.xml")
@@ -17,13 +25,22 @@ public class UserTest {
 	@Resource
 	private UserService userService;
 	@Resource
-	private UserImpl2 userImpl;
-	@Test
+	private IUser iUser;
+	//@Test
 	public void test() {
 		//userService.add();
 		User user = new User();
 		user.setPassword("123456");
 		user.setUserName("mm");
-		userImpl.save(user);
+		iUser.save(user);
 	}
+	@Test
+	public void findByUserName() {
+		Sort sort = new Sort(Sort.Direction.ASC,"id").and(new Sort(Sort.Direction.DESC,"userName"));
+		Pageable pageable = new PageRequest(0, 2,sort);
+		
+		Page<User> page = iUser.findByUserName("mm", pageable);
+		System.out.println(JSONObject.fromObject(page).toString());
+	}
+	
 }
